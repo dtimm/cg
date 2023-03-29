ca2cfd45608eac64434dcd1eb0aaa9e1fab6e6ab
David Timm <dtimm@pm.me>
2023-03-29 09:13:41 -0600

    wip: adding conversation mode

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type options struct {
 	APIToken    string `short:"t" long:"api-token" env:"OPENAI_API_TOKEN" description:"OpenAI API token"`
 	Interactive bool   `short:"i" long:"interactive" description:"start in interactive mode"`
 	Prompt      string `short:"p" long:"prompt" description:"prompt to pass to OpenAI"`
-	// OutFile     string `short:"o" long:"out-file" description:"file to write chat to (append only)"`
+	OutFile     string `short:"o" long:"out-file" description:"file to write chat to (append only)"`
 }
 
 func main() {
@@ -23,29 +23,21 @@ func main() {
 
 	client := openai.NewClient(opt.APIToken)
 
-	messages := []openai.ChatCompletionMessage{}
+	if opt.OutFile != "" {
+		f, err := os.Stat(opt.OutFile)
+	}
+
+	printIt := func(s string) {
+		fmt.Println(s)
+
+	}
+	var messages []openai.ChatCompletionMessage
+	var err error
 
 	if opt.Interactive {
-		scanner := bufio.NewScanner(os.Stdin)
-		for scanner.Scan() {
-			fmt.Printf("user: ")
-			m := openai.ChatCompletionMessage{
-				Role:    openai.ChatMessageRoleUser,
-				Content: scanner.Text(),
-			}
-			messages = append(messages, m)
-			resp, err := client.CreateChatCompletion(
-				context.Background(),
-				openai.ChatCompletionRequest{
-					Model:    openai.GPT3Dot5Turbo,
-					Messages: messages,
-				})
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "%s", err)
-				break
-			}
-			messages = append(messages, resp.Choices[0].Message)
-			fmt.Printf("agent: %s\n\n", resp.Choices[0].Message.Content)
+		messages, err = interactive(client)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s", err)
 		}
 	} else {
 		resp, err := single(client, opt.Prompt)
@@ -53,27 +45,56 @@ func main() {
 			fmt.Fprintf(os.Stderr, "%s", err)
 		}
 
-		fmt.Println(resp)
+		messages = []openai.ChatCompletionMessage{resp}
 	}
 
 	fmt.Printf("\n\n\n%#v\n\n", messages)
 	os.Exit(0)
 }
 
-func single(c *openai.Client, p string) (string, error) {
+func packageUserPrompt(p string) openai.ChatCompletionMessage {
+	return openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleUser,
+		Content: p,
+	}
+}
+
+func single(c *openai.Client, p string) (openai.ChatCompletionMessage, error) {
+	m := packageUserPrompt(p)
 	resp, err := c.CreateChatCompletion(
 		context.Background(),
 		openai.ChatCompletionRequest{
-			Model: openai.GPT3Dot5Turbo,
-			Messages: []openai.ChatCompletionMessage{{
-				Role:    openai.ChatMessageRoleUser,
-				Content: p,
-			}},
+			Model:    openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{m},
 		},
 	)
 	if err != nil {
-		return "", fmt.Errorf("error in CreateChatCompletion: %s", err)
+		return openai.ChatCompletionMessage{}, fmt.Errorf("error in CreateChatCompletion: %s", err)
 	}
 
-	return resp.Choices[0].Message.Content, nil
+	return resp.Choices[0].Message, nil
+}
+
+func interactive(c *openai.Client) ([]openai.ChatCompletionMessage, error) {
+	messages := []openai.ChatCompletionMessage{}
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Printf("user: ")
+		m := packageUserPrompt(scanner.Text())
+		messages = append(messages, m)
+		resp, err := c.CreateChatCompletion(
+			context.Background(),
+			openai.ChatCompletionRequest{
+				Model:    openai.GPT3Dot5Turbo,
+				Messages: messages,
+			})
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s", err)
+			break
+		}
+
+		messages = append(messages, resp.Choices[0].Message)
+		fmt.Printf("agent: %s\n\n", resp.Choices[0].Message.Content)
+	}
+	return messages, nil
 }
